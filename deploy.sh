c957339fce287de9fa2f49f63b66e7878c251dc7
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-12-18 17:16:24 +0000

    Changes done

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+cd /home/pc17-dpl/travelroad_laravel
+git add .
+git commit -m "Changes done"
+git push
+
 ssh dimas@dimas.arkania.es "
-  cd $(dirname $0)
+  cd /home/dimas/travelroad_laravel
   git pull
   composer install
 "
